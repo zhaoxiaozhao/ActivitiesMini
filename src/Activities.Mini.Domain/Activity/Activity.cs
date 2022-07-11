68cda93ecfd7201033f126d2e2e706271d438eff
赵昌友 <zhaochangyou@hotmail.com>
2022-07-11 23:07:47 +0800

    add entities

---
+++ src/Activities.Mini.Domain/Activity/Activity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Volo.Abp.Domain.Entities;
+
+namespace Activities.Mini.Activity;
+
+public class Activity: AggregateRoot<long>
+{
+    public string CoverUrl { get; set; }
+    public string Subject { get; protected set; }
+    public string Content { get; protected set; }
+    public DateTime? StartTime { get; protected set; }
+    public DateTime? EndTime { get; protected set; }
+    public long Creator { get; protected set; }
+    public string Address { get; protected set; }
+    public List<Attender> Attenders { get; protected set; }
+
+    protected Activity() { }
+    public Activity(string subject, string conent)
+    {
+        Subject = subject;
+        Content = conent;
+        Attenders = new List<Attender>();
+    }
+    public void SetStartTime(DateTime? start) { StartTime = start; }
+    public void SetEndTime(DateTime? end) { EndTime = end; }
+    public void AddAttender(long attenderId)
+    {
+        if (attenderId == 0) return;
+
+        Attenders.Add(new Attender(attenderId));
+    }
+}
---
+++ src/Activities.Mini.Domain/Activity/Attender.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+
+namespace Activities.Mini.Activity
+{
+    public class Attender : Entity
+    {
+        public long AttenderId { get; set; }
+        public string AttenderName { get; set; }
+        public int Gender { get; set; }
+        public int Age { get; set; }
+        protected Attender() { }
+        public Attender(long attenderId)
+        {
+            AttenderId = attenderId;
+        }
+        public override object[] GetKeys()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
